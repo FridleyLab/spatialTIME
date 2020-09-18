bde6c6b87f2137e1c73af5f6e31b4ff81a72712e
jhcreed <jordan.h.creed@moffitt.org>
2020-09-18 17:10:44 -0400

    set up for simulating data

--- R/utils-helpers.R
+++ R/utils-helpers.R
@@ -1,10 +1,66 @@
 # Dark theme for plotting
 theme_dark_mode <- function () { 
   ggdark::dark_mode() %+replace% 
-    theme(
-      axis.title = element_blank(),
-      axis.text = element_blank(),
-      axis.ticks = element_blank(),
-      panel.grid = element_blank()
+    ggplot2::theme(
+      axis.title = ggplot2::element_blank(),
+      axis.text = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank()
     )
+}
+
+# positive cells/stroma for simulating IF data 
+modes_stroma <- function(k, xmin = 0, xmax = 10, ymin = 0, ymax = 10, sdmin = 1/2, sdmax = 2){ 
+  center_peak <- data.frame(x = stats::runif(k,xmin,xmax), 
+                           y = stats::runif(k,ymin,ymax),
+                           sd_x = stats::runif(k,sdmin,sdmax), 
+                           sd_y = stats::runif(k, sdmin, sdmax))
+  
+  center_peak <- center_peak %>% 
+    dplyr::rowwise() %>%
+    dplyr::mutate(rho = runif(1, -sd_x * sd_y, sd_x * sd_y))
+  
+  return(center_peak)
+  
+}
+
+#Generate random holes for simulating IF data 
+hole <- function(xmin = xmin, xmax = xmax, ymin = ymin, 
+                ymax = ymax, sdmin = sdmin, sdmax = sdmax){
+  #Random select the percent to total area missing
+  percent_area_missing <- runif(1, 0.2, 0.35)
+  #Random select the number of holes, can range from 1 to floor(percent_area_missing*10)
+  num_holes <- sample(2:floor(percent_area_missing*10), 1)
+  if(percent_area_missing < 0.2 | num_holes == 1){
+    num_holes <- 1
+    area_allocation <- percent_area_missing
+  }else{
+    area_allocation <- Surrogate::RandVec(a = 0.1, 
+                                          b = percent_area_missing, 
+                                          s = percent_area_missing,
+                                          n = num_holes)$RandVecOutput
+  }
+  
+  center_hole <- data.frame(x = runif(num_holes, xmin, xmax),
+                            y = runif(num_holes, ymin, ymax),
+                            # rho = 0,
+                            sd_x = runif(num_holes, sdmin, sdmax), 
+                            sd_y = runif(num_holes, sdmin, sdmax))
+  center_hole <- center_hole %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(rho = runif(1, -sd_x * sd_y, sd_x * sd_y)) %>% 
+    dplyr::bind_cols(area = area_allocation[,1]) %>% 
+    dplyr::mutate(max_dist = sqrt(area * (xmax - xmin) * (ymax - ymin) / pi))
+  
+  return(center_hole)
+  
+}
+
+dist_function <- function(a){
+  diff <- c((pp$x[i] - center_peak[a,1]), (pp$y[i] - center_peak[a,2]))
+  diff <- unlist(diff)
+  sigma <- matrix(c(center_peak$sd_x[a]^2, rep(center_peak$rho[a], 2),
+                    center_peak$sd_y[a]^2), nrow = 2, ncol = 2)
+  z <- t(diff) %*% solve(sigma) %*% diff
+  closest <- exp(-z)
 }
