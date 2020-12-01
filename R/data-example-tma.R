e41c9e11b94e71bed831e0904c2d31af85ceed38
jhcreed <jordan.h.creed@moffitt.org>
2020-12-01 17:25:15 -0500

    create example data

---
+++ R/data-example-tma.R
@@ -0,0 +1,18 @@
+#' Example list of spatial TMA data 
+#'
+#' A list containing 10 spatial data frames 
+#'
+#' @format A list of 10 data frames: 
+#' \itemize{
+#' \item TMA_[T,3].tiff
+#' \item TMA_[I,7].tiff
+#' \item TMA_[W,5].tiff
+#' \item TMA_[N,1].tiff
+#' \item TMA_[D,2].tiff
+#' \item TMA_[C,6].tiff
+#' \item TMA_[F,6].tiff
+#' \item TMA_[F,3].tiff
+#' \item TMA_[P,5].tiff
+#' \item TMA_[T,9].tiff
+#' }
+"example_tma"
