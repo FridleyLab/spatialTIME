c49ff99013473fe61023ad9834e6ef29e5979cce
jhcreed <jordan.h.creed@moffitt.org>
2021-01-04 16:28:16 -0500

    new naming conventions

--- R/data-example-tma.R
+++ R/data-example-tma.R
@@ -4,15 +4,15 @@
 #'
 #' @format A list of 10 data frames: 
 #' \itemize{
-#' \item TMA_[T,3].tiff
-#' \item TMA_[I,7].tiff
-#' \item TMA_[W,5].tiff
-#' \item TMA_[N,1].tiff
-#' \item TMA_[D,2].tiff
-#' \item TMA_[C,6].tiff
-#' \item TMA_[F,6].tiff
-#' \item TMA_[F,3].tiff
-#' \item TMA_[P,5].tiff
-#' \item TMA_[T,9].tiff
+#' \item TMA_\[T,3\].tiff
+#' \item TMA_\[I,7\].tiff
+#' \item TMA_\[W,5\].tiff
+#' \item TMA_\[N,1\].tiff
+#' \item TMA_\[D,2\].tiff
+#' \item TMA_\[C,6\].tiff
+#' \item TMA_\[F,6\].tiff
+#' \item TMA_\[F,3\].tiff
+#' \item TMA_\[P,5\].tiff
+#' \item TMA_\[T,9\].tiff
 #' }
 "example_tma"
