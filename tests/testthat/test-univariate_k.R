c1e57db31e0e3db8c5776caee14c93737964df9d
jhcreed <jordan.h.creed@moffitt.org>
2021-01-04 15:58:46 -0500

    fix expected length

--- tests/testthat/test-univariate_k.R
+++ tests/testthat/test-univariate_k.R
@@ -6,10 +6,16 @@ test_that("returns proper number of elements ", {
   # grabbing image tag names to make sample and clinical file -----
   spatial_names <- lapply(example_tma, function(x) {x$image.tag[[1]]})
   spatial_names <- unlist(spatial_names)
-  spatial_names <- gsub(".tif", "", spatial_names)
+  spatial_names <- gsub("\\.tif", "", spatial_names)
+  
+  example_tma <- lapply(example_tma, function(x){
+    x <- x %>% janitor::clean_names()
+  })
+  
+  names(example_tma) <- spatial_names
   
   set.seed(8675309)
-  example_sample <- data.frame(image.tag = spatial_names,
+  example_sample <- data.frame(image_tag = spatial_names,
                                patient_id = sample(c("patient_x", "patient_y", "patient_z"),
                                                    10, replace = TRUE))
   
@@ -17,12 +23,14 @@ test_that("returns proper number of elements ", {
                                  covar_one = c("low", "high", "low"),
                                  covar_two = rnorm(3))
   
-  x <- create_mif(example_tma, clinical_data = example_clinical, sample_data = example_sample)
+  x <- create_mif(spatial_list = example_tma, 
+                  clinical_data = example_clinical, 
+                  sample_data = example_sample)
+  
   mnames <- c("foxp3_opal_620_positive", "cd3_opal_570_positive", "cd8_opal_520_positive",
               "pd1_opal_650_positive", "pdl1_opal_540_positive")
   
   y <- ripleys_k(x, id = "image_tag", mnames = mnames, calculation = "theoretical")
   
-  expect_equal(length(y), 10)
-  expect_equal(length(y[[1]]), length(mnames))
+  expect_equal(nrow(y), length(mnames)*length(x$spatial))
 })
