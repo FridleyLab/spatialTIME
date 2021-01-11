4e9fc575d19530df50e1ae14611fb4469bc44c5f
jhcreed <jordan.h.creed@moffitt.org>
2021-01-11 12:58:27 -0500

    dependent on clinical file

--- R/create_mif.R
+++ R/create_mif.R
@@ -1,62 +1,43 @@
 #' Create Multiplex Immunoflourescent object 
 #'
 #' @description Creates an MIF object for use in spatialIF functions
+#' @param clinical_data A data frame containing patient level data with one row
+#' per participant. 
+#' @param sample_data A data frame containing sample level data with one row per 
+#' sample. Should at a minimum contain a 2 columns: one for sample names and 
+#' one for the corresponding patient name.
 #' @param spatial_list A named list of data frames with the spatial data from 
-#' each sample making up each individual data frame
-#' @param clinical_data A data frame containing patient level data. Patient ID
-#' indicated with variable `patient_id`
-#' @param sample_data A data frame containing sample level data. Sample ID 
-#' should be indicated with variable `image_tag` while patient ID should 
-#' be indicated with variable  `patient_id`. 
+#'  each sample making up each individual data frame
+#' @param patient_id A character string indicating the column name for patient id in 
+#'  sample and clinical data frames. 
+#' @param sample_id A character string indicating the column name for sample id
+#'  in the sample data frame
 #' @param clean_columns A logical value indicating if names in spatial data frames
-#' should be rewritten in a cleaner format. Default is TRUE. 
+#'  should be rewritten in a cleaner format. Default is TRUE. 
 #' 
 #' @return Returns a custom MIF
-#'    \item{spatial}{Named list of spatial data}
 #'    \item{clinical}{Data frame of clinical data}
 #'    \item{sample}{Data frame of sample data}
+#'    \item{spatial}{Named list of spatial data}
 #'    \item{derived}{List of data derived using the MIF object}
 #'    
 #' @export
-create_mif <- function(spatial_list, clinical_data = NULL, sample_data = NULL,
+create_mif <- function(clinical_data, sample_data, spatial_list = NULL,
+                       patient_id = "patient_id", sample_id = "image_tag",
                        clean_columns = TRUE){
   
-  # if spatial data list is unnamed - name each element according to 
-  # image tag (does every file come with image.tage and is it always named
-  # "image.tag")
-  if(is.null(names(spatial_list))) {
-    spatial_names <- lapply(spatial_list, function(x) {x$image_tag[[1]]})
-    spatial_names <- unlist(spatial_names)
-    spatial_names <- gsub("\\.tiff", "", spatial_names)
-    
-    names(spatial_list) <- spatial_names
-  }
-
-  # create empty data frames assuming a single sample per patient
-  if(is.null(clinical_data)){
-    clinical_data <- data.frame(patient_id = names(spatial_list))
-  }
-  if(is.null(sample_data)){
-    sample_data <- data.frame(image_tag = names(spatial_list),
-                              patient_id = names(spatial_list))
-  }
+  sample_data_clean <- sample_data %>% 
+    dplyr::full_join(clinical_data %>% 
+                dplyr::select(!!patient_id), by = patient_id) %>% 
+    dplyr::select(!!patient_id, !!sample_id, dplyr::everything())
   
-  # clean names in clinical and sample data? 
-  # have the column name be "image.tag" ?
-  spatial_sample_names <- intersect(sample_data[["image_tag"]], 
-                                    names(spatial_list))
-  
-  clinical_sample_names <- intersect(sample_data[["patient_id"]],
-                                     clinical_data[["patient_id"]])
-  
-  sample_data <- sample_data %>% 
-    dplyr::filter(.data$image_tag %in% paste0(spatial_sample_names,".tif") | 
-                    .data$patient_id %in% clinical_sample_names)
-  
-  clinical_data <- clinical_data %>%
-    dplyr::filter(.data$patient_id %in% clinical_sample_names)
-  
-  spatial_list <- spatial_list[spatial_sample_names]
+  clinical_data_clean <- clinical_data %>% 
+    dplyr::full_join(sample_data %>% 
+                dplyr::group_by_at(dplyr::vars(patient_id)) %>% 
+                dplyr::mutate(sample_string = paste0(!!(as.name(sample_id)), collapse = "|")) %>% 
+                dplyr::select(!!patient_id, .data$sample_string) %>% 
+                dplyr::slice(1), by = patient_id) %>%
+    dplyr::select(!!patient_id, .data$sample_string, dplyr::everything())
   
   if(clean_columns== TRUE){
     sample_data <- sample_data %>% 
@@ -65,16 +46,32 @@ create_mif <- function(spatial_list, clinical_data = NULL, sample_data = NULL,
     clinical_data <- clinical_data %>% 
       janitor::clean_names()
     
-    spatial_list <- lapply(spatial_list, 
-                           function(x) {janitor::clean_names(x)})
+    if(!is.null(spatial_list)){
+      
+      spatial_list <- lapply(spatial_list, 
+                             function(x) {janitor::clean_names(x)})
+      
+    }
+    
   }
   
-  mif <- list(spatial = spatial_list,
-              clinical = clinical_data,
-              sample = sample_data,
-              derived = list())
+  if(!is.null(spatial_list) & is.null(names(spatial_list))){
+    
+    spatial_names <- lapply(spatial_list, function(x) {x[[sample_id]][[1]]})
+    spatial_names <- unlist(spatial_names)
+    
+    names(spatial_list) <- spatial_names
+    
+  }
   
-  # class(mif) <- c("mif", class(mif))
+  if(is.null(spatial_list)) {
+    spatial_list <- list(NA)
+  }
+
+  mif <- list(clinical = clinical_data_clean,
+              sample = sample_data_clean,
+              spatial = spatial_list,
+              derived = list())
   
   structure(mif, class="mif")
   
--- R/print.R
+++ R/print.R
@@ -4,11 +4,11 @@ print.mif <- function(x, ...){
   # create color schemes for output text 
   emphesis <- crayon::make_style("deepskyblue")
   
-  x$sample <- x$sample %>% 
-    janitor::clean_names()
+  # x$sample <- x$sample %>% 
+  #   janitor::clean_names()
   
-  cat(emphesis(length(unique(x$clinical$patient_id))), "patients spanning",
-      emphesis(length(unique(x$sample$image_tag))), "samples and",
+  cat(emphesis(length(unique(x$clinical[,1]))), "patients spanning",
+      emphesis(length(unique(x$sample[,2]))), "samples and",
       emphesis(length(x$spatial)), "spatial data frames were found \n")
   # add number/names of genes in a panel
 }
