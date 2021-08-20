fff8aac33e5c4fc0e7c3ba60d3517da292a6ddd9
cwilso6 <christopher.wilson@moffitt.org>
2021-08-20 15:13:05 -0400

    Subsets spatial and updates summary data

--- R/subset_mif.R
+++ R/subset_mif.R
@@ -7,24 +7,38 @@
 #' @param mif An MIF object
 #' @param classifier Column name for spatial dataframe to subset
 #' @param level Determines which level of the classifier to keep.
-#' 
+#' @param markers vector of 
 #' @return mif object where the spatial list only as the cell that are the specified level.
 #'    
 #' @export
 #'
-
-subset_mif = function(mif, classifier, level){
+subset_mif = function(mif, classifier, level, markers){
   split_spatial = list()
-  
+  summary = data.frame()
   for(a in 1:length(mif$spatial)){
-    tmp = mif$spatial[[a]] %>% filter(get(classifier) == level)
+    tmp = mif$spatial[[a]] %>% dplyr::filter(get(classifier) == level)
     if(nrow(tmp)>2){
       split_spatial = rlist::list.append(split_spatial, tmp)
       names(split_spatial)[length(split_spatial)] = tmp[[mif$sample_id]][1]
+      percent = tmp %>% 
+        dplyr::select(!!markers) %>% 
+        dplyr::summarize_all(~sum(.)) %>%
+        dplyr::mutate_all(.funs = ~./nrow(tmp))
+      colnames(percent) = paste0(level, ': % ', colnames(percent))
+      counts = tmp %>% 
+        dplyr::select(!!markers) %>% 
+        dplyr::summarize_all(~sum(.)) %>%
+        dplyr::mutate(`Total Cells` = nrow(tmp))
+      colnames(counts) = paste0(level, ': ', colnames(counts))
+      out = c(tmp[[mif$patient_id]][1], 
+              tmp[[mif$sample_id]][1],
+              unlist(counts) , unlist(percent))
+      names(out)[1:2] = c(mif$patient_id,mif$sample_id)
     }
+    summary = rbind.data.frame(summary, t(out))
   }
   
-  mif_new = create_mif(clinical_data = mif$clinical, sample_data = mif$sample,
+  mif_new = create_mif(clinical_data = mif$clinical, sample_data = summary,
                        spatial_list = split_spatial, patient_id =  mif$patient_id, 
                        sample_id =  mif$sample_id)
   
