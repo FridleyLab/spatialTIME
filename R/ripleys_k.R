f08b8ad2181d51943f335f606438d58d29b4ffa0
ACSoupir <alex.soupir@jacks.sdstate.edu>
2023-12-18 10:58:20 -0500

    forgot radii for isotropic adjustment - fixed

--- R/ripleys_k.R
+++ R/ripleys_k.R
@@ -111,7 +111,8 @@ ripleys_k = function(mif,
       if(edge_correction %in% c("translation", "trans")){
         edge = spatstat.explore::edge.Trans(spatstat.geom::ppp(x = spat$xloc, y = spat$yloc, window = win), W = win)
       } else if(edge_correction %in% c("isotropic", "iso")){
-        edge = spatstat.explore::edge.Ripley(spatstat.geom::ppp(x = spat$xloc, y = spat$yloc, window = win))
+        edge = spatstat.explore::edge.Ripley(spatstat.geom::ppp(x = spat$xloc, y = spat$yloc, window = win), 
+                                             r = pairdist(spatstat.geom::ppp(x = spat$xloc, y = spat$yloc, window = win)))
       } else if(edge_correction == "none"){
         edge = matrix(nrow = nrow(spat), ncol = nrow(spat), data = 1)
       }
